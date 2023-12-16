b7a34e0cfcff393e528eb5ed9552268de9843ab8
harriz <danial.harriz360@gmail.com>
2023-12-16 16:57:14 +0800

    fixed the code

--- Submission/sec02/DTD/Assignment1/Assignment1.cpp
+++ Submission/sec02/DTD/Assignment1/Assignment1.cpp
@@ -1,93 +1,128 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
 #include <fstream>
+#include <string>
+#define N 16
 
 using namespace std;
-
-const int MAX_BOOKS = 10; 
 
 class Book {
 private:
     string title;
     string author;
-    int publicationYear;
+    int year;
     string ISBN;
 
 public:
-    // Default Constructor
-    Book() : title(""), author(""), publicationYear(0), ISBN("") {}
+    Book() {
+        title = "";
+        author = "";
+        year = 0;
+        ISBN = "";
+    }
 
-    // Parameterized Constructor
-    Book(const string& t, const string& a, int year, const string& isbn)
-        : title(t), author(a), publicationYear(year), ISBN(isbn) {}
+    string getTitle() const {
+        return title;
+    }
 
-    // Accessors
-    string getTitle() const { return title; }
-    string getAuthor() const { return author; }
-    int getPublicationYear() const { return publicationYear; }
-    string getISBN() const { return ISBN; }
+    string getAuthor() const {
+        return author;
+    }
 
-    // Display method
-    void displayBook() const {
-        cout << "Title: " << title << ", Author: " << author
-             << ", Year: " << publicationYear << ", ISBN: " << ISBN << endl;
+    int getYear() const {
+        return year;
+    }
+
+    string getISBN() const {
+        return ISBN;
+    }
+
+    void setTitle(string t) {
+        title = t;
+    }
+
+    void setAuthor(string a) {
+        author = a;
+    }
+
+    void setYear(int y) {
+        year = y;
+    }
+
+    void setISBN(string i) {
+        ISBN = i;
     }
 };
 
-// Function to swap two books
-void swapBooks(Book& a, Book& b) {
-    Book temp = a;
-    a = b;
-    b = temp;
+int readFile(Book book[], int& size) { // read data from file
+    ifstream inputFile("Book.txt");
+    string title, author, isbn;
+    int year;
+    int i = 0;
+
+    if (inputFile.is_open()) {
+        while (getline(inputFile, title, ',') && getline(inputFile >> ws, author, ',') && (inputFile >> year, true)) {
+            inputFile.ignore();
+            getline(inputFile >> ws, isbn);
+            book[i].setTitle(title);
+            book[i].setAuthor(author);
+            book[i].setYear(year);
+            book[i].setISBN(isbn);
+            i++;
+        }
+        inputFile.close();
+    } else {
+        cout << "Unable to open file" << endl;
+    }
+
+    size = i;
+    return size;
 }
 
-// Function to sort books based on user's choice using bubble sort
-void sortBooks(Book books[], int size, int option) {
-    switch (option) {
-        case 1:
-            for (int i = 0; i < size - 1; ++i) {
-                for (int j = 0; j < size - i - 1; ++j) {
-                    if (books[j].getTitle() > books[j + 1].getTitle()) {
-                        swapBooks(books[j], books[j + 1]);
-                    }
-                }
+// Sorting
+void sortByTitle(Book books[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (books[i].getTitle() > books[j].getTitle()) {
+                swap(books[i], books[j]);
             }
-            break;
-        case 2:
-            for (int i = 0; i < size - 1; ++i) {
-                for (int j = 0; j < size - i - 1; ++j) {
-                    if (books[j].getAuthor() > books[j + 1].getAuthor()) {
-                        swapBooks(books[j], books[j + 1]);
-                    }
-                }
-            }
-            break;
-        case 3:
-            for (int i = 0; i < size - 1; ++i) {
-                for (int j = 0; j < size - i - 1; ++j) {
-                    if (books[j].getPublicationYear() > books[j + 1].getPublicationYear()) {
-                        swapBooks(books[j], books[j + 1]);
-                    }
-                }
-            }
-            break;
-        case 4:
-            for (int i = 0; i < size - 1; ++i) {
-                for (int j = 0; j < size - i - 1; ++j) {
-                    if (books[j].getISBN() > books[j + 1].getISBN()) {
-                        swapBooks(books[j], books[j + 1]);
-                    }
-                }
-            }
-            break;
-        default:
-            cout << "Invalid option" << endl;
+        }
     }
 }
 
-// Function to print books in tabular format
-void printBooks(const Book books[], int size) {
+void sortByAuthor(Book books[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (books[i].getAuthor() > books[j].getAuthor()) {
+                swap(books[i], books[j]);
+            }
+        }
+    }
+}
+
+void sortByYear(Book books[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (books[i].getYear() > books[j].getYear()) {
+                swap(books[i], books[j]);
+            }
+        }
+    }
+}
+
+void sortByISBN(Book books[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (books[i].getISBN() > books[j].getISBN()) {
+                swap(books[i], books[j]);
+            }
+        }
+    }
+}
+
+// Print
+void printBook(Book books[], int size) {
+    system("cls");
     cout << "<<< Sorted Books >>>" << endl;
     cout << "-----------------------------------------------------------------------------------------------------------" << endl;
     cout << setw(50) << left << "| Title"
@@ -98,108 +133,64 @@ void printBooks(const Book books[], int size) {
     for (int i = 0; i < size; ++i) {
         cout << setw(50) << left << "| " + books[i].getTitle()
              << setw(30) << left << "| " + books[i].getAuthor()
-             << setw(10) << left << "| " + to_string(books[i].getPublicationYear())
+             << setw(10) << left << "| " + to_string(books[i].getYear())
              << setw(15) << left << "| " + books[i].getISBN() + "|" << endl;
     }
     cout << "-----------------------------------------------------------------------------------------------------------" << endl;
 }
 
-// Function to search for a book based on user's choice
-void searchBooks(const Book books[], int size, int option, const string& key) {
-    switch (option) {
+int main() {
+    Book books[N];
+    int choice, sortChoice;
+    int size = 0;
+
+    cout << setw(40) << " ____________________________________" << endl;
+    cout << setw(40) << "|                                    |" << endl;
+    cout << setw(40) << "|   Welcome to Sort/Search by DTD!   |" << endl;
+    cout << setw(40) << "|____________________________________|" << endl << endl;
+
+    cout << setw(15) << "[1] Sorting" << endl;
+    cout << setw(17) << "[2] Searching" << endl << endl;
+
+    cout << "Please enter your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        system("cls");
+        cout << setw(40) << " ____________________________________" << endl;
+        cout << setw(40) << "|                                    |" << endl;
+        cout << setw(40) << "|              Sorting               |" << endl;
+        cout << setw(40) << "|____________________________________|" << endl << endl;
+        cout << setw(15) << "[1] Sort by Title" << endl;
+        cout << setw(17) << "[2] Sort by Author" << endl;
+        cout << setw(16) << "[3] Sort by Year" << endl;
+        cout << setw(16) << "[4] Sort by ISBN" << endl << endl;
+
+        cout << "Please enter your choice: ";
+        cin >> sortChoice;
+
+        readFile(books, size);
+
+        switch (sortChoice) {
         case 1:
-            // Search by title
-            for (int i = 0; i < size; ++i) {
-                if (books[i].getTitle() == key) {
-                    books[i].displayBook();
-                    return;
-                }
-            }
-            cout << "Book with title '" << key << "' not found." << endl;
+            sortByTitle(books, size);
             break;
         case 2:
-            // Implement search logic for other options  author)
+            sortByAuthor(books, size);
             break;
         case 3:
-            // Implement search logic for other options by publication year)
+            sortByYear(books, size);
             break;
         case 4:
-            // Implement search logic for other options by ISBN)
+            sortByISBN(books, size);
             break;
-
         default:
-            cout << "Invalid option" << endl;
-    }
-}
-
-int main() {
-    // Read books from file
-    ifstream inputFile("book.txt");
-    Book books[MAX_BOOKS];
-    int numBooks = 0;
-
-    if (inputFile.is_open()) {
-        string title, author, isbn;
-        int year;
-
-        while (getline(inputFile, title, ',') &&
-               getline(inputFile >> ws, author, ',') &&
-            (inputFile >> year, true) &&
-               getline(inputFile >> ws, isbn)) {
-            books[numBooks++] = Book(title, author, year, isbn);
+            cout << "Invalid choice" << endl;
+            break;
         }
-        inputFile.close();
-    } else {
-        cerr << "Unable to open file" << endl;
-        return 1;
+
+        cout << "Sorted books: " << endl;
+        printBook(books, size);
     }
-
-    // Calculate the actual number of books in the array
-    numBooks = sizeof(books) / sizeof(books[0]);
-
-    // Display menu for user to choose between sorting and searching
-    int operationOption;
-    cout << "<<< Library Management System >>>" << endl;
-    cout << "[1] Sort Books" << endl;
-    cout << "[2] Search Books" << endl;
-    cout << "Option: ";
-    cin >> operationOption;
-
-    if (operationOption == 1) {
-        // Display menu for sorting
-        int sortingOption;
-        cout << "<<< Sorting Process >>>" << endl;
-        cout << "[1] By title" << endl;
-        cout << "[2] By author" << endl;
-        cout << "[3] By publication year" << endl;
-        cout << "[4] By ISBN" << endl;
-        cout << "Option: ";
-        cin >> sortingOption;
-
-        // Sort books based on user's choice
-        sortBooks(books, numBooks, sortingOption);
-
-        // Print the sorted books
-        printBooks(books, numBooks);
-    } else if (operationOption == 2) {
-        // Display menu for searching
-        int searchingOption;
-        string searchKey;
-        cout << "<<< Searching Process >>>" << endl;
-        cout << "[1] By title" << endl;
-        cout << "[2] By author" << endl;
-        cout << "[3] By publication year" << endl;
-        cout << "[4] By ISBN" << endl;
-        cout << "Option: ";
-        cin >> searchingOption;
-        cout << "Enter search key: ";
-        cin >> searchKey;
-
-        // Search for a book based on user's choice
-        searchBooks(books, numBooks, searchingOption, searchKey);
-    } else {
-        cout << "Invalid option" << endl;
-    }
-
     return 0;
 }
