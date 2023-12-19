70e07964ac8c6830551e7f1f5df2dcff5b6418df
Muhammad Abduh Bin Abdul Ba'ari <147372941+MuhdAbduh@users.noreply.github.com>
2023-12-19 23:06:10 +0800

    Update ass1 test.cpp

--- Submission/sec04/FABS/Ass1/ass1 test.cpp
+++ Submission/sec04/FABS/Ass1/ass1 test.cpp
@@ -73,7 +73,7 @@ public:
 };
 
 
-int searchString(string key, Courier arr[], int size) {
+int SearchString(string key, Courier arr[], int size) {
     for (int i = 0; i < size; i++) {
 	if (arr[i].getName() == key || arr[i].getType() == key || arr[i].getSource() == key ||
 	    arr[i].getDest() == key || arr[i].getStat() == key) {
@@ -173,10 +173,10 @@ void quickSortStr(Courier arr[], int first, int last, bool sortByType) {
 	if (first < last) {
 		int pivotIndex;
 		if (sortByType) {
-			pivotIndex = partitionStrByType(arr, first, last);
+			pivotIndex = partitionSortByType(arr, first, last);
 		}
 		else {
-			pivotIndex = partitionStrByName(arr, first, last);
+			pivotIndex = partitionSortByName(arr, first, last);
 		}
 
 	quickSortStr(arr, first, pivotIndex - 1, sortByType);
@@ -195,7 +195,6 @@ int main() {
 
     int choice, index, intKey;;
     string key;
-    int index = -1; // Move the declaration outside the switch statement
 
     do {
         cout << "Menu:\n"
@@ -268,12 +267,13 @@ int main() {
 			case 2: {
 				cout << "\nEnter the number to search: ";
 				cin >> intKey;
-				index = SearchInt(intKey, couriers, size);
+				index = searchInt(intKey, couriers, size);
 				break;
 			}
 			default:
 			cout << "Invalid search type choice.\n";
 			index = -1;
+		}
                		if (index != -1) {
         			cout << "\nCourier found at list number " << index+1 << ":\n";
        				cout << "\n| Name              | Parcel Type   | Source         | Destination    | Status          | Tracking Number |\n"
@@ -296,7 +296,7 @@ int main() {
             default:
                 cout << "Invalid choice. Please enter a valid option.\n";
         }
-
+	
     } while (choice != 4);
 
     return 0;
