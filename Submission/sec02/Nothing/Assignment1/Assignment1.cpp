0843f77d0e93afb7317ea09258cdb312634d987c
GOH JING YANG <129203250+jingyang26@users.noreply.github.com>
2023-12-19 10:04:27 +0800

    Update Assignment1.cpp

--- Submission/sec02/Nothing/Assignment1/Assignment1.cpp
+++ Submission/sec02/Nothing/Assignment1/Assignment1.cpp
@@ -15,3 +15,41 @@ class inventory{
         char[] itemLocation; //rack A01, B02, etc.
 
 
+void merge(inventory inv[], int low, int mid, int high){
+    int i, j, k;
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    inventory L[n1], R[n2];
+    for(i = 0; i < n1; i++){
+        L[i] = inv[low + i];
+    }
+    for(j = 0; j < n2; j++){
+        R[j] = inv[mid + 1 + j];
+    }
+    i = 0;
+    j = 0;
+    k = low;
+    while(i < n1 && j < n2){
+        if(L[i].getItemNumber() <= R[j].getItemNumber()){
+            inv[k] = L[i];
+            i++;
+        }
+        else{
+            inv[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i < n1){
+        inv[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j < n2){
+        inv[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+
