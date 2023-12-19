9f70765703c6e5df83f123f89afafa9d36cea0e0
Aeron Goh Ming Lun <147372922+AeronGohMingLun@users.noreply.github.com>
2023-12-19 16:10:27 +0000

    class Menu

--- Submission/sec04/Boboboy/Assignment1.cpp
+++ Submission/sec04/Boboboy/Assignment1.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#define N 100
+
 using namespace std;
 
+class Menu{
+	private:
+		string code,name, foodtype;
+		float price;
+	public:
+		Menu(string c = " ", string n = " ", string f = " ", float p =0.0 ){
+			code = c;
+			name = n;
+			foodtype = f;
+			price = p;
+		}
+		string getcode(){
+			return code;
+		}
+		string getname(){
+			return name;
+		}
+		string getfoodtype(){
+			return foodtype;
+		}
+		float getprice(){
+			return price;
+		}
+		void setcode(string c){
+			code = c;
+		}
+		void setname(string n){
+			name = n;
+		}
+		void settype(string t){
+			foodtype = t;
+		}
+		void setprice(float p){
+			price = p;
+		}
+		void printmenu(){
+		cout << code << setw(20) << name << setw(20) << foodtype << setw(20) << price << endl;		
+		}
+};
 int main(){
     
 }
