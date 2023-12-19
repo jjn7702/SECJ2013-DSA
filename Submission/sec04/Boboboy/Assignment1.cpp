8f45356e94be67cf8d31b20a083aa8632dcb83a5
TayShunWei <147373066+TayShunWei@users.noreply.github.com>
2023-12-19 16:47:43 +0000

    class for mergeSort

--- Submission/sec04/Boboboy/Assignment1.cpp
+++ Submission/sec04/Boboboy/Assignment1.cpp
@@ -45,6 +45,78 @@ class Menu{
 		cout << code << setw(20) << name << setw(20) << foodtype << setw(20) << price << endl;		
 		}
 };
+
+void merge(Menu theArray[], int first, int mid, int last,int o) {
+    Menu tempArray[N];
+    int first1 = first;
+    int last1 = mid;
+    int first2 = mid + 1;
+    int last2 = last;
+    int index = first1;
+    if(o == 1){
+    for (; (first1 <= last1) && (first2 <= last2); ++index) {
+        if (theArray[first1].getcode() < theArray[first2].getcode()) {
+            tempArray[index] = theArray[first1];
+            ++first1;
+        } else {
+            tempArray[index] = theArray[first2];
+            ++first2;
+        }
+    }
+	}
+	else if(o == 2){
+	for (; (first1 <= last1) && (first2 <= last2); ++index) {
+        if (theArray[first1].getcode() > theArray[first2].getcode()) {
+            tempArray[index] = theArray[first1];
+            ++first1;
+        } else {
+            tempArray[index] = theArray[first2];
+            ++first2;
+        }
+    }
+	}
+	else if(o == 3){
+	for (; (first1 <= last1) && (first2 <= last2); ++index) {
+        if (theArray[first1].getname() < theArray[first2].getname()) {
+            tempArray[index] = theArray[first1];
+            ++first1;
+        } else {
+            tempArray[index] = theArray[first2];
+            ++first2;
+        }
+    }
+	}
+	
+	else if(o == 4){
+	for (; (first1 <= last1) && (first2 <= last2); ++index) {
+        if (theArray[first1].getname() > theArray[first2].getname()) {
+            tempArray[index] = theArray[first1];
+            ++first1;
+        } else {
+            tempArray[index] = theArray[first2];
+            ++first2;
+        }
+    }
+	}
+    for (; first1 <= last1; ++first1, ++index)
+        tempArray[index] = theArray[first1];
+
+    for (; first2 <= last2; ++first2, ++index)
+        tempArray[index] = theArray[first2];
+
+    for (index = first; index <= last; ++index)
+        theArray[index] = tempArray[index];
+    }
+
+void mergeSort(Menu theArray[], int first, int last,int o) {
+    if (first < last){
+        int mid = (first + last) / 2;
+        mergeSort(theArray, first, mid,o);
+        mergeSort(theArray, mid+1, last,o);
+        merge(theArray, first, mid, last, o);
+    }
+}
+
 int main(){
     string menu_code, menu_name, menu_type;
 	float menu_price;
