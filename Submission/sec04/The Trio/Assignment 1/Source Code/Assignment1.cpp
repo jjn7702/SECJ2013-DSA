3bea66830e9a03e426be584aa4a3477ee2370547
Hafizah Jafri <147373870+hafizahjafri@users.noreply.github.com>
2023-12-20 09:12:56 +0000

    searchKey

--- Submission/sec04/The Trio/Assignment 1/Source Code/Assignment1.cpp
+++ Submission/sec04/The Trio/Assignment 1/Source Code/Assignment1.cpp
@@ -343,6 +343,48 @@ int menuSearch()
 	return option ;
 }
 
+// Search key based on name, IC number, phone number or check in date
+void Search(string searchKey, Customer data[], int size)
+{ 
+	int index = -1 ; //-1 means record is not found
+	for(int i = 0; i < size; i++)
+	{ 
+		if(searchKey == data[i].getName())
+		{ 
+			index = i; // assign current array index
+			break; //terminate searching
+		}
+		else if(searchKey == data[i].getIC())
+		{ 
+			index = i; // assign current array index
+			break; //terminate searching
+		}
+		else if(searchKey == data[i].getPhone())
+		{ 
+			index = i; // assign current array index
+			break; //terminate searching
+		}
+		else if(searchKey == data[i].getDate())
+		{ 
+			index = i; // assign current array index
+			break; //terminate searching
+		}	
+	} //end for
+	
+	if(index == -1) // If the key not found
+		cout << "\nNot found..." << endl ;
+	else
+	{	// Display the information after searching if found
+		cout << "\nName: " << data[index].getName() << endl
+			 << "Age: " << data[index].getAge() << endl
+	 		 << "IC Number: " << data[index].getIC() << endl
+			 << "Phone Number: " << data[index].getPhone() << endl 
+			 << "Room Number: " << data[index].getNum() << endl 
+			 << "CheckInDate: " << data[index].getDate() << endl ;	
+	}	
+} //end of search for name, IC number, phone number, check in date
+
+
 // Search key based on age or room number
 void Search2(int searchKey, Customer data[], int size)
 { 
