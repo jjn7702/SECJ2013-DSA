54e43698b036a9fb85bebd7ad99bfd202cc53f79
Thevan Raju Jeganath <thevanrajujeganath@gmail.com>
2023-12-24 20:37:19 +0800

    Update Assignment2.cpp

--- Submission/sec02/DTD/Assignment2/Assignment2.cpp
+++ Submission/sec02/DTD/Assignment2/Assignment2.cpp
@@ -187,3 +187,135 @@ void Library::displayList() const {
 
     cout << "-----------------------------------------------------------------------------------------------------------" << endl;
 }
+
+int main() {
+    Library library;
+    int choice;
+    char yn1;
+    string Title, Author, isbn;
+    int Year;
+
+    ifstream inputFile("book.txt");
+
+    // Check if the file is open
+    if (!inputFile.is_open()) {
+        cerr << "Error opening file: book.txt" << endl;
+        return 1; // Exit with an error code
+    }
+
+    // Read books from the file and add them to the library
+    int booksRead = 0;
+
+    while (!inputFile.eof()) {
+    getline(inputFile, Title, ',');
+    getline(inputFile, Author, ',');
+    inputFile >> Year;
+    inputFile.ignore(); // Consume the newline character after reading the year
+    getline(inputFile, isbn);
+    Book newBook;
+    newBook.setTitle(Title);
+    newBook.setAuthor(Author);
+    newBook.setYear(Year);
+    newBook.setISBN(isbn);
+    library.addNode(newBook);
+    booksRead++;
+}
+
+    cout << "Number of books read: " << booksRead << endl;
+
+    // Close the file
+    inputFile.close();
+
+    do {
+        system("cls");
+        cout << setw(39) << " ____________________________________" << endl;
+        cout << setw(40) << "|                                    |" << endl;
+        cout << setw(40) << "|       Welcome to DTD Library!      |" << endl;
+        cout << setw(40) << "|____________________________________|" << endl << endl;
+
+        cout << setw(5) << "[1] Add Book" << endl;
+        cout << setw(5) << "[2] Find Book" << endl;
+        cout << setw(5) << "[3] Delete Book" << endl;
+        cout << setw(5) << "[4] Sort Books" << endl;
+        cout << setw(5) << "[5] Display Books" << endl;
+        cout << setw(5) << "[6] Exit" << endl << endl;
+
+        cout << "Please enter your choice: ";
+
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                // Add Book
+                Book newBook;
+                cin.ignore(); // Ignore newline character from previous input
+                cout << "Enter title: ";
+                getline(cin, Title);
+                newBook.setTitle(Title);
+                cout << "Enter author: ";
+                getline(cin, Author);
+                newBook.setAuthor(Author);
+                cout << "Enter year: ";
+                cin >> Year;
+                newBook.setYear(Year);
+                cin.ignore(); // Ignore newline character from previous input
+                cout << "Enter ISBN: ";
+                getline(cin, isbn);
+                newBook.setISBN(isbn);
+
+                library.addNode(newBook);
+                cout << "Book added successfully!" << endl;
+                break;
+            }
+
+            case 2: {
+                // Find Book
+                string searchKey;
+                cout << "Enter title or ISBN to search: ";
+                cin >> searchKey;
+                Node* foundNode = library.findNode(searchKey);
+                if (foundNode != nullptr) {
+                    cout << "Book found: " << endl;
+                    foundNode->data.displayBook();
+                } else {
+                    cout << "Book not found with title or ISBN: " << searchKey << endl;
+                }
+                break;
+            }
+            case 3: {
+                // Delete Book
+                string deleteKey;
+                cout << "Enter title or ISBN to delete: ";
+                cin >> deleteKey;
+                library.deleteNode(deleteKey);
+                cout << "Book deleted successfully!" << endl;
+                break;
+            }
+            case 4:
+                // Sort Books
+                library.sortList();
+                cout << "Books sorted successfully!" << endl;
+                break;
+            case 5:
+                // Display Books
+                library.displayList();
+                break;
+            case 6:
+                // Exit
+                cout << "Exiting the program. Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+
+        cout << "---------------------------------" << endl;
+
+        cout << "Do you still want to use the program? (Y/N): ";
+        cin >> yn1;
+
+    } while (yn1 == 'Y' || yn1 == 'y');
+
+    cout << "Thank you for using the program!" << endl;
+
+    return 0;
+}
