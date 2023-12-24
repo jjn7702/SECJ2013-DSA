1a114c59656941cd7ace65cab04ba1bee7c4b57f
Thevan Raju Jeganath <thevanrajujeganath@gmail.com>
2023-12-24 20:31:57 +0800

    class Book

--- Submission/sec02/DTD/Assignment2/Assignment2.cpp
+++ Submission/sec02/DTD/Assignment2/Assignment2.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+class Book {
+private:
+    string title;
+    string author;
+    int year;
+    string ISBN;
+
+public:
+    Book() {
+        title = "";
+        author = "";
+        year = 0;
+        ISBN = "";
+    }
+
+    string getTitle() const {
+        return title;
+    }
+
+    string getAuthor() const {
+        return author;
+    }
+
+    int getYear() const {
+        return year;
+    }
+
+    string getISBN() const {
+        return ISBN;
+    }
+
+    void setTitle(const string& t) { 
+        title = t;
+    }
+
+    void setAuthor(const string& a) { 
+        author = a; 
+    }
+
+    void setYear(int y) { 
+        year = y; 
+    }
+
+    void setISBN(const string& isbn) { 
+        ISBN = isbn; 
+    }
+
+    // Display book details
+    void displayBook() const {
+        cout << setw(50) << left << "| " + title
+             << setw(30) << left << "| " + author
+             << setw(10) << left << "| " + to_string(year)
+             << setw(15) << left << "| " + ISBN + "|" << endl;
+    }
+};
