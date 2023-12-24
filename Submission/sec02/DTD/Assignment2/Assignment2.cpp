c29512f7a52b2c50b253bec4f42b2ef168500e41
Thevan Raju Jeganath <thevanrajujeganath@gmail.com>
2023-12-24 20:36:27 +0800

    Node and class Library

--- Submission/sec02/DTD/Assignment2/Assignment2.cpp
+++ Submission/sec02/DTD/Assignment2/Assignment2.cpp
@@ -60,3 +60,130 @@ public:
              << setw(15) << left << "| " + ISBN + "|" << endl;
     }
 };
+
+// Node structure for the linked list
+struct Node {
+    Book data;
+    Node* next;
+};
+
+class Library {
+private:
+    Node* head;
+
+public:
+    // Constructor and destructor
+    Library() : head(nullptr) {}
+    ~Library();
+
+    // Linked list operations
+    void addNode(const Book& book);
+    void deleteNode(const string& key);
+    Node* findNode(const string& key) const;
+    void sortList();
+    void displayList() const;
+};
+
+Library::~Library() {
+    // Destructor to free allocated memory
+    Node* current = head;
+    Node* next;
+    while (current != nullptr) {
+        next = current->next;
+        delete current;
+        current = next;
+    }
+}
+
+void Library::addNode(const Book& book) {
+    Node* newNode = new Node{book, nullptr};
+
+    // Insert at the beginning
+    newNode->next = head;
+    head = newNode;
+}
+
+void Library::deleteNode(const string& key) {
+    Node* current = head;
+    Node* prev = nullptr;
+
+    // Traverse the list to find the node with the key
+    while (current != nullptr && (current->data.getTitle() != key && current->data.getISBN() != key)) {
+        prev = current;
+        current = current->next;
+    }
+
+    // If the node with the key is found
+    if (current != nullptr) {
+        if (prev == nullptr) {
+            // If it's the first node
+            head = current->next;
+        } else {
+            // If it's in the middle or end
+            prev->next = current->next;
+        }
+        delete current;
+    } else {
+        cout << "Book not found with title or ISBN: " << key << endl;
+    }
+}
+
+Node* Library::findNode(const string& key) const {
+    Node* current = head;
+
+    while (current != nullptr && (current->data.getTitle() != key && current->data.getISBN() != key)) {
+        current = current->next;
+    }
+
+    return current;
+}
+
+void Library::sortList() {
+    // Simple bubble sort for sorting by title in ascending order
+    if (head == nullptr || head->next == nullptr) {
+        // Already sorted or empty list
+        return;
+    }
+
+    bool swapped;
+    Node* current;
+    Node* lastSorted = nullptr;
+
+    do {
+        swapped = false;
+        current = head;
+
+        while (current->next != lastSorted) {
+            if (current->data.getTitle() > current->next->data.getTitle()) {
+                // Swap data
+                Book temp = current->data;
+                current->data = current->next->data;
+                current->next->data = temp;
+                swapped = true;
+            }
+
+            current = current->next;
+        }
+
+        lastSorted = current;
+
+    } while (swapped);
+}
+
+void Library::displayList() const {
+    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
+    cout << setw(50) << left << "| Title"
+         << setw(30) << left << "| Author"
+         << setw(10) << left << "| Year"
+         << setw(15) << left << "| ISBN" << " |" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
+
+    Node* current = head;
+
+    while (current != nullptr) {
+        current->data.displayBook();
+        current = current->next;
+    }
+
+    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
+}
