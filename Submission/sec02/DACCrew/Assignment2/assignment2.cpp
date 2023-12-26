2a2e02094c704db8d8bfb2e98459a91030afd5ee
Nur Farah Adibah <128114912+nurfarahadibah@users.noreply.github.com>
2023-12-26 20:46:46 +0800

    Update assignment2.cpp

--- Submission/sec02/DACCrew/Assignment2/assignment2.cpp
+++ Submission/sec02/DACCrew/Assignment2/assignment2.cpp
@@ -1,3 +1,9 @@
+// Assignment 2 - SECJ2013 - 23241 (assignment2.cpp)
+// Group Members:
+// 1. Nur Farah Adibah Binti Idris A22EC0245
+// 2. 
+// 3.
+
 #include <iostream>
 #include <string>
 #include <iomanip>
