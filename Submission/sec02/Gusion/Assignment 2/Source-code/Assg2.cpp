d957bd91bd64d4aad6be91ce5a51b4e7481e999c
Che Marhumi <128233086+CheMrhumi@users.noreply.github.com>
2023-12-27 23:27:29 +0800

    KELANTAN BANJIR

--- Submission/sec02/Gusion/Assignment 2/Source-code/Assg2.cpp
+++ Submission/sec02/Gusion/Assignment 2/Source-code/Assg2.cpp
@@ -11,7 +11,6 @@
 
 using namespace std;
 
-
 // Task (Chemi) --------> Function LinkedList & int main (Part LinkedList)
 ///-----------------------------------------------CLASS CUSTOMER----------------------------------------------------------------///
 class Cust
@@ -153,7 +152,7 @@ public:
         if (position < 1 || position >= updatedSize())
         {
             cout << "\n\t\t\t\t\t\tInvalid position." << endl;
-            cout <<"\t\t\t\t\t\tCannot add customer." << endl;
+            cout << "\t\t\t\t\t\tCannot add customer." << endl;
             return;
         }
 
@@ -278,9 +277,8 @@ public:
     }
     ///-----------------------------------------------END OF FUNCTION TO DELETE CUSTOMER----------------------------------------------------------------///
 
-
-// Task (Ariff) --------> Function Searching & int main (Part Searching)
-  vector<Node *> findNode(const string &searchKey, int searchType)
+    // Task (Ariff) --------> Function Searching & int main (Part Searching)
+    vector<Node *> findNode(const string &searchKey, int searchType)
     {
         Node *current = head;
         vector<Node *> matchNode;
@@ -288,15 +286,15 @@ public:
         while (current != nullptr)
         {
             bool isMatch = false;
-            
-            //convert the key to uppercase or lowercase when user accidently enter wrong char
+
+            // convert the key to uppercase or lowercase when user accidently enter wrong char
             string lowerSearchKey = searchKey;
             transform(lowerSearchKey.begin(), lowerSearchKey.end(), lowerSearchKey.begin(), ::tolower);
 
             string lowerName = current->data->getName();
             transform(lowerName.begin(), lowerName.end(), lowerName.begin(), ::tolower);
 
-            if (searchType == 1 && lowerName.find(lowerSearchKey) != string::npos )
+            if (searchType == 1 && lowerName.find(lowerSearchKey) != string::npos)
             {
                 isMatch = true;
             }
@@ -343,9 +341,9 @@ public:
         return matchNode;
     }
 
-// Task (Iman) --------> Function Sorting & int main (Part Sorting)
+    // Task (Iman) --------> Function Sorting & int main (Part Sorting)
 
- void bubbleSortByName()
+    void bubbleSortByName()
     {
         bool swapped;
         Node *ptr1;
@@ -442,11 +440,180 @@ public:
 
 int main()
 {
+    ///-------------------------------MARHUMI START---------------------///
+    LinkedList custList;
+    string line;
+    int count, size;
 
+    fstream fileIn("booking.txt", ios::in);
 
-    
-                                                //Task (Ariff) 
-///-----------------------------------------------CODE TO START THE SEARCHING IN THE LIST----------------------------------------------------------------///
+    if (!fileIn)
+    {
+        cout << "File input/output error!\n";
+        return 1;
+    }
+    else
+    {
+        while (getline(fileIn, line))
+        {
+            // Use stringstream to parse the line
+            stringstream ss(line);
+            string name, destination, airlines, dateStr;
+            int day, month, years;
+
+            // Read the name
+
+            ss >> name;
+            while (ss >> ws && !isdigit(ss.peek()))
+            {
+                // Append subsequent words to the name
+                string word;
+                ss >> word;
+                name += ' ' + word;
+            }
+
+            // Read the date and other details
+            ss >> day;
+            ss.ignore(); // Ignore the dot
+            ss >> month;
+            ss.ignore(); // Ignore the dot
+            ss >> years;
+
+            ss >> destination >> airlines;
+
+            if (!ss.fail())
+            {
+                // Adding/extract infomation from booking.txt to list
+                custList.addNode(new Cust(name, day, month, years, destination, airlines));
+                size++; // edit
+            }
+            else
+            {
+                cout << "Invalid date format in line: " << line << endl;
+            }
+        }
+    }
+
+    int position; // edit
+    int choice;
+
+    custList.getInitialSize(size - 2);
+    do
+    {
+        cout << "\n\t\t\t\t   +================== MAIN MENU ===================+" << endl;
+        cout << "\t\t\t\t   +1. \t\tAdd a new customer"
+             << "\t\t    +" << endl;
+        cout << "\t\t\t\t   +2. \t\tDelete a customer"
+             << "\t\t    +" << endl;
+        cout << "\t\t\t\t   +3. \t\tFind a customer"
+             << "\t\t\t    +" << endl;
+        cout << "\t\t\t\t   +4. \t\tSort the list"
+             << "\t\t\t    +" << endl;
+        cout << "\t\t\t\t   +5. \t\tDisplay all customer"
+             << "\t\t    +" << endl;
+        cout << "\t\t\t\t   +6. \t\tExit"
+             << "\t\t\t\t    +" << endl;
+        cout << "\t\t\t\t   +================================================+" << endl;
+        cout << "\n\n\t\t\t\t\t\tEnter your choice: ";
+        cin >> choice;
+        cout << endl;
+
+        switch (choice)
+        {
+        ///-----------------------------------------------CODE TO ADD CUSTOMER TO THE LIST----------------------------------------------------------------///
+        case 1:
+        {
+            // Add a new node
+            // Provide options for adding at the beginning, middle, or end
+            int addChoice;
+            cout << "\t\t\t\t\t\t1. Add at the beginning\n\t\t\t\t\t\t2. Add at the middle\n\t\t\t\t\t\t3. Add at the end\n\n\t\t\t\t\t\tEnter your choice: ";
+            cin >> addChoice;
+
+            string newName, newDestination, newAirlines;
+            int newDay, newMonth, newYear;
+
+            cout << "\n\t\t\t\t\t\tEnter name: ";
+            cin.ignore(); // Clear the newline character from the previous input
+            getline(cin, newName);
+
+            cout << "\n\t\t\t\t\t\tEnter date (day month year): " << endl;
+            cout << "\n\t\t\t\t\t\tDay (eg : 12): ";
+            cin >> newDay;
+            cout << "\t\t\t\t\t\tMonth (eg : 6) : ";
+            cin >> newMonth;
+            cout << "\t\t\t\t\t\tYear (eg : 2002): ";
+            cin >> newYear;
+
+            cout << "\n\t\t\t\t\t\tEnter destination" << endl;
+            cout << "\t\t\t\t\t\t(Attention! Case Sensitive): ";
+            cin >> newDestination;
+
+            cout << "\n\t\t\t\t\t\tEnter airlines" << endl;
+            cout << "\t\t\t\t\t\t(Attention! Case Sensitive): ";
+            cin >> newAirlines;
+
+            Cust *newCust = new Cust(newName, newDay, newMonth, newYear, newDestination, newAirlines);
+
+            switch (addChoice)
+            {
+            case 1:
+                custList.addToBeginning(newCust);
+                cout << "\n\t\t\t\t\t\tCustomer added successfully." << endl;
+                break;
+            case 2:
+                // int position;
+                cout << "\n\t\t\t\t\t\tAdd after position: ";
+                cin >> position;
+                custList.addToMiddle(newCust, position);
+                break;
+            case 3:
+                custList.addToEnd(newCust);
+                cout << "\n\t\t\t\t\t\tCustomer added successfully." << endl;
+                break;
+            default:
+                cout << "\t\t\t\t\t\tInvalid choice." << endl;
+            }
+
+            break;
+        }
+        ///-----------------------------------------------END OF CODE TO ADD CUSTOMER TO THE LIST----------------------------------------------------------------///
+
+        ///-----------------------------------------------CODE TO DELETE CUSTOMER IN THE LIST----------------------------------------------------------------///
+        case 2:
+        {
+            // Delete a node
+            // Provide options for deleting from the beginning, middle, or end
+            int deleteChoice;
+            cout << "\t\t\t\t\t\t1. Delete from the beginning\n\t\t\t\t\t\t2. Delete from the middle\n\t\t\t\t\t\t3. Delete from the end\n\n\t\t\t\t\t\tEnter your choice: ";
+            cin >> deleteChoice;
+
+            switch (deleteChoice)
+            {
+            case 1:
+                custList.deleteFromBeginning();
+                cout << "\n\t\t\t\t\t\tCustomer deleted successfully." << endl;
+                break;
+            case 2:
+                // int position;
+                cout << "\n\t\t\t\t\t\tEnter position: ";
+                cin >> position;
+                custList.deleteFromMiddle(position);
+                break;
+            case 3:
+                custList.deleteFromEnd();
+                cout << "\n\t\t\t\t\t\tCustomer deleted successfully." << endl;
+                break;
+            default:
+                cout << "\n\t\t\t\t\t\t[Invalid choice.]!!!!" << endl;
+            }
+
+            break;
+        }
+            ///-----------------------------------------------END OF CODE TO DELETE CUSTOMER TO THE LIST----------------------------------------------------------------///
+            ///--------------------------END MARHUMI-----------------///
+
+            // Task (Ariff)
+            ///-----------------------------------------------CODE TO START THE SEARCHING IN THE LIST----------------------------------------------------------------///
         case 3: // search
         {
             int searchKey;
@@ -633,27 +800,30 @@ int main()
             } while (searchKey != 5);
         }
         break;
-        ///--------------------------------------------------------END OF CODE TO SEARCH----------------------------------------------------------------------------------///
- ///-------------------------------------------------------- CODE TO SORT THE LIST----------------------------------------------------------------------------------///
+            ///--------------------------------------------------------END OF CODE TO SEARCH----------------------------------------------------------------------------------///
+           
+            ///-------------------------------------------------------- CODE TO SORT THE LIST----------------------------------------------------------------------------------///
         case 4:
-            // Sort the list           
+            // Sort the list
             custList.bubbleSort();
             cout << "\n\t\t\t\t\t\tList sorted successfully." << endl;
             break;
-        ///--------------------------------------------------------END OF CODE TO SORT THE LIST----------------------------------------------------------------------------------///
-     ///-------------------------------------------------------- CODE TO DISPLAY LIST----------------------------------------------------------------------------------///
+            ///--------------------------------------------------------END OF CODE TO SORT THE LIST----------------------------------------------------------------------------------///
+           
+            ///-------------------------------------------------------- CODE TO DISPLAY LIST----------------------------------------------------------------------------------///
         case 5:
             cout << "\t\t\t----------------------------------------------------------------------------" << endl;
             cout << left << setw(5) << "\t\t\tNo." << setw(15) << "  Name"
                  << "  Date of Booking"
                  << "\t     Destination"
                  << "\tAirlines" << endl;
-            cout << "\t\t\t----------------------------------------------------------------------------" << endl;;
+            cout << "\t\t\t----------------------------------------------------------------------------" << endl;
+            ;
             custList.listcust();
             cout << "\t\t\t----------------------------------------------------------------------------" << endl;
             cout << endl;
             break;
-        ///--------------------------------------------------------END OF CODE TO DISPLAY----------------------------------------------------------------------------------///
+            ///--------------------------------------------------------END OF CODE TO DISPLAY----------------------------------------------------------------------------------///
 
         case 6:
             // Exit
@@ -665,6 +835,6 @@ int main()
     } while (choice != 6);
 
     system("pause");
-            
-return 0;
+
+    return 0;
 }
