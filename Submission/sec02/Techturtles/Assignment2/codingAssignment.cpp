59899e10b418b6416f17782063714f2048aafd6c
safwan031 <safwan03@graduate.utm.my>
2023-12-29 23:35:47 +0800

    Update codingAssignment.cpp

--- Submission/sec02/Techturtles/Assignment2/codingAssignment.cpp
+++ Submission/sec02/Techturtles/Assignment2/codingAssignment.cpp
@@ -2,4 +2,38 @@
 #include <fstream>
 #include <iomanip>
 #include <stdlib.h>
+
 using namespace std;
+
+class courier
+{
+private:
+    string trackNum;
+    string senderName;
+    string receiverName;
+    string deliveryStatus;
+
+public:
+    courier(string track, string sender, string receiver, string status) : trackNum(track), senderName(sender), receiverName(receiver), deliveryStatus(status) {}
+
+    string getTrackingNumber()
+    {
+        return trackNum;
+    }
+    string getSenderName()
+    {
+        return trackNum;
+    }
+    string getReceiverName()
+    {
+        return trackNum;
+    }
+    string getDeliveryStatus()
+    {
+        return trackNum;
+    }
+    string getTrackingNumber()
+    {
+        return trackNum;
+    }
+}
