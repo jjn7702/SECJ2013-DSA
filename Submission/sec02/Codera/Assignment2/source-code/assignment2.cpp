92a7d408edbe2252ea3f3e198b9e0d7490256315
Weng <118497269+zw-weng@users.noreply.github.com>
2023-12-30 02:49:12 +0800

    Update assignment2.cpp

--- Submission/sec02/Codera/Assignment2/source-code/assignment2.cpp
+++ Submission/sec02/Codera/Assignment2/source-code/assignment2.cpp
@@ -1,3 +1,10 @@
+//Assignment 2
+//Group Codera
+//Hospital Management System
+//Neo Zheng Weng A22EC0093
+//Joseph Lau Yeo Kai A22EC0055
+//Lee Yik Hong A21BE0376
+
 #include <iostream>
 #include <fstream>
 #include <string>
