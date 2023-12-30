4b8ccbfe016717dcc113a828de5462056a67017c
Kuganes Varman <147373287+KuganesVarman@users.noreply.github.com>
2023-12-30 06:41:05 +0000

    InsAtNode2

--- Submission/sec04/Agent P/ass2/Assignment_2.cpp
+++ Submission/sec04/Agent P/ass2/Assignment_2.cpp
@@ -116,7 +116,10 @@ class List
         }
 
         void insertAtNode(string n,string f,int a,string ic,string e,string m,int index)
-        {}
+        {
+            Voter *newNode = new Voter(n,f,a,ic,e,m);
+            Voter *temp = head;
+        }
 
         
 void search(int opt,string key,int key2)
