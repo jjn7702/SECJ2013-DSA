ad313bc9a81351671e704cc811f129a66b36db36
TayShunWei <147373066+TayShunWei@users.noreply.github.com>
2023-12-31 07:49:38 +0000

    upload the space of the output

--- Submission/sec04/Boboboy/Assignment 2/Source code/Assignment 2.cpp
+++ Submission/sec04/Boboboy/Assignment 2/Source code/Assignment 2.cpp
@@ -188,9 +188,9 @@ class Cart{
 				cout << "Cart is empty" << endl;
 				return;
 			}
-			cout << "************ CART ************" << endl << endl;
+			cout << "********************** Receipt *******************************" << endl << endl;
 			cout << "-------------------------------------------------------------" << endl;
-			cout << "Code" << setw(17) << "Name" << setw(20) << "Type" << setw(20) << "Price(RM)" << endl;
+			cout << "Code" << setw(18) << "Name" << setw(20) << "Type" << setw(20) << "Price(RM)" << endl;
 			cout << "-------------------------------------------------------------" << endl;
 			while(temp){
 				cout << temp->getcode() << setw(20) << temp->getname() << setw(20) << temp->getfoodtype() << setw(20) << temp->getprice() << endl;
