7083534f0cb2ddfe121734481c43330117e471b2
Eddy-Koh <147373165+Eddy-Koh@users.noreply.github.com>
2024-01-13 09:23:25 +0000

    Array Stack

--- Submission/sec04/The Trio/Project/Source Code/Project.cpp
+++ Submission/sec04/The Trio/Project/Source Code/Project.cpp
@@ -4,3 +4,14 @@
 #include <iomanip>
 using namespace std ;
 
+class Stack{  // used by hotel manager
+	private:
+		Customer cust[100] ;
+		int top ;
+	
+	public:
+		Stack(){ top = -1 ; }	// set Top to NULL
+		bool isEmpty(){ return top == -1 ; } // check whether stack is empty
+		bool isFull(){ return top == 99 ; } // check whether stack is full
+
+};
