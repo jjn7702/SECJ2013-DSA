8139c96b4d44f06ec93d42a9daba4073d7c3a0ec
Eddy-Koh <147373165+Eddy-Koh@users.noreply.github.com>
2024-01-13 09:24:24 +0000

    push stack function

--- Submission/sec04/The Trio/Project/Source Code/Project.cpp
+++ Submission/sec04/The Trio/Project/Source Code/Project.cpp
@@ -14,4 +14,37 @@ class Stack{  // used by hotel manager
 		bool isEmpty(){ return top == -1 ; } // check whether stack is empty
 		bool isFull(){ return top == 99 ; } // check whether stack is full
 
+        void processBookingRequest(Queue& queue) // push
+		{	
+			Customer* firstCust = queue.getFirstReservation() ;  // get the fist customer for reservation list
+			
+			if(firstCust == NULL)
+			{
+				cout << "\nNo reservation available to process" << endl ;
+			}
+			else
+			{
+				Customer *reserveList = new Customer(*firstCust);
+				
+				if(isFull())
+					cout << "The hotel booking request is full" << endl ;
+				else
+				{
+					top++ ;
+					cust[top].setName(reserveList->getName()) ;
+					cust[top].setAge(reserveList->getAge()) ;
+					cust[top].setIC(reserveList->getIC()) ;
+					cust[top].setPhone(reserveList->getPhone()) ;
+					cust[top].setNum(reserveList->getNum()) ;
+					cust[top].setDate(reserveList->getDate()) ;
+					
+					cout << "\nYou have accepted " << reserveList->getName() << "'s booking request" << endl ;
+				
+					queue.DoneBooking() ;
+				}	
+
+			}
+		}
+
+        
 };
