03507b3919cec832455c93ba0d6a17f92c61e81c
Hafizah Jafri <147373870+hafizahjafri@users.noreply.github.com>
2024-01-14 03:21:50 +0000

    class queue

--- Submission/sec04/The Trio/Project/Source Code/Project.cpp
+++ Submission/sec04/The Trio/Project/Source Code/Project.cpp
@@ -4,6 +4,25 @@
 #include <iomanip>
 using namespace std ;
 
+class Queue{  // used by agent
+	private:
+		Customer *front, *back ;
+	
+	public:
+		Queue(){ front = NULL ; back = NULL ; }
+		bool isEmpty(){ return ( (front == NULL) && (back == NULL) ) ; }
+		
+		Customer* getFirstReservation() // Get the first customer in the reservation list
+		{ 
+			if(isEmpty())
+			{
+				cout << "\nThe reservation list is empty" << endl ;
+				return NULL ;
+			}
+			else
+				return front ; 
+		}
+
 class Stack{  // used by hotel manager
 	private:
 		Customer cust[100] ;
