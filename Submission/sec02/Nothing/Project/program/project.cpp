8aeeff9681bdb1dff7a123ddf1916fb4f69c2890
LOW JIE SHENG <129199459+jiesheng4616@users.noreply.github.com>
2024-01-15 21:18:30 +0800

    Update project.cpp

--- Submission/sec02/Nothing/Project/program/project.cpp
+++ Submission/sec02/Nothing/Project/program/project.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+class Inventory{
+    private:
+        int itemNumber;
+        int quantity;
+        double cost;
+        double totalCost;
+    public:
+        Inventory();
+        Inventory(int, int, double);
+        void setItemNumber(int);
+        void setQuantity(int);
+        void setCost(double);
+        void setTotalCost();
+        int getItemNumber();
+        int getQuantity();
+        double getCost();
+        double getTotalCost();
+};
