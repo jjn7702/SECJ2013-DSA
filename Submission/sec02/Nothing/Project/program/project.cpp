aa54c293fb0506bb0789b5d05cbd57d0bd910667
GOH JING YANG <129203250+jingyang26@users.noreply.github.com>
2024-01-16 22:56:30 +0800

    Update project.cpp

--- Submission/sec02/Nothing/Project/program/project.cpp
+++ Submission/sec02/Nothing/Project/program/project.cpp
@@ -96,3 +96,77 @@ class itemQueue{
                 return front->item;
         }
 }
+
+class historyNodeStack{
+    public:
+        goods item;
+        char action;
+        historyNodeStack *next;
+        historyNodeStack *prev;
+        
+}
+
+class historyStack{
+    private:
+        historyNodeStack *top;
+        historyNodeStack *bottom;
+        int size;
+
+    public:
+        void createStack(){
+            top = NULL;
+            bottom = NULL;
+            size = 0;
+        }
+
+        bool isEmpty(){
+            if(top == NULL)
+                return true;
+            else
+                return false;
+        }
+
+        void push(goods item, char action){
+            historyNodeStack *newNode = new historyNodeStack;
+            newNode->item = item;
+            newNode->action = action;
+            newNode->next = NULL;
+            newNode->prev = NULL;
+
+            if(isEmpty()){
+                top = newNode;
+                bottom = newNode;
+            }else{
+                newNode->prev = top;
+                top->next = newNode;
+                top = newNode;
+            }
+            size++;
+        }
+
+        void pop(){
+            if(top == NULL){
+                cout << "Stack is empty" << endl;
+            }else{
+                historyNodeStack *temp = top;
+                top = top->prev;
+                top->next = NULL;
+                delete temp;
+                size--;
+            }
+        }
+
+        char getAction(){
+            if(isEmpty())
+                cout << "Stack is empty" << endl;
+            else
+                return top->action;
+        }
+
+        goods getItem(){
+            if(isEmpty())
+                cout << "Stack is empty" << endl;
+            else
+                return top->item;
+        }   
+}
