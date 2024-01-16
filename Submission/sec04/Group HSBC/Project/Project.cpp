80bff45f3afcd690b8715f62fcdde1ef5c503345
Chan Qing Yee <108549486+currylaksa@users.noreply.github.com>
2024-01-16 07:40:54 +0000

    starting

--- Submission/sec04/Group HSBC/Project/Project.cpp
+++ Submission/sec04/Group HSBC/Project/Project.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <vector>
 
+using namespace std;
+
+int main()
+{
+    
+    system("pause");
+    return 0;
+}
