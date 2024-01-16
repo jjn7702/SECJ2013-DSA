ee0318287ef70a6b3947b1323077ae2abbc78a8a
Chong Hui <147372986+LinChongHui@users.noreply.github.com>
2024-01-16 13:26:07 +0000

    add class Order

--- Submission/sec04/Boboboy/Project/Project.cpp
+++ Submission/sec04/Boboboy/Project/Project.cpp
@@ -44,3 +44,45 @@ class Menu{
 		cout << code << setw(20) << name << setw(20) << foodtype << setw(20) << price << endl;		
 		}
 };
+
+class Order{
+	Menu menu;
+	string code,name, foodtype;
+	float price;
+	int counter_num;
+		
+	public:
+		Order *next;
+		Order(string c ,string n, string f ,float p){
+			code = c;
+			name = n;
+			foodtype = f;
+			price = p;
+			counter_num = 1;
+			next = NULL;
+		}
+		string getcode(){
+			return code;
+		}
+		string getname(){
+			return name;
+		}
+		string getfoodtype(){
+			return foodtype;
+		}
+		float getprice(){
+			return price;
+		}
+		void setcode(string c){
+			code = c;
+		}
+		void setname(string n){
+			name = n;
+		}
+		void settype(string f){
+			foodtype = f;
+		}
+		void setprice(float p){
+			price = p;
+		}
+};
