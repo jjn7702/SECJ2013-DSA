81e7c70ba18f395b2db53cfb3e46fa2a03430683
Aeron Goh Ming Lun <147372922+AeronGohMingLun@users.noreply.github.com>
2024-01-16 13:19:24 +0000

    header and class menu

--- Submission/sec04/Boboboy/Project/Project.cpp
+++ Submission/sec04/Boboboy/Project/Project.cpp
@@ -1,2 +1,42 @@
-#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#define N 14
 using namespace std;
+
+class Menu{
+	private:
+		string code,name, foodtype;
+		float price;
+	public:
+		Menu(string c = " ",string n = " ", string f = " ", float p =0.0 ){
+			code = c;
+			name = n;
+			foodtype = f;
+			price = p;
+		}
+		string getcode(){
+			return code;
+		}
+		string getname(){
+			return name;
+		}
+		string getfoodtype(){
+			return foodtype;
+		}
+		float getprice(){
+			return price;
+		}
+		void setcode(string c){
+			code = c;
+		}
+		void setname(string n){
+			name = n;
+		}
+		void settype(string f){
+			foodtype = f;
+		}
+		void setprice(float p){
+			price = p;
+		}
+		void printmenu(){
