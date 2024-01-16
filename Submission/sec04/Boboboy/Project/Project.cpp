3ea004089d95502adb8ec90eab0fc07d89d61411
Aeron Goh Ming Lun <147372922+AeronGohMingLun@users.noreply.github.com>
2024-01-16 13:45:04 +0000

    add bubble sort for name in ascending and descending

--- Submission/sec04/Boboboy/Project/Project.cpp
+++ Submission/sec04/Boboboy/Project/Project.cpp
@@ -291,3 +291,33 @@ class Cart{
             lptr = ptr1;
         } while (swapped);
     }
+     else if(way == 3){
+        do {
+            swapped = 0;
+            ptr1 = head;
+
+            while (ptr1->next != lptr) {
+                if (ptr1->getname() > ptr1->next->getname()) {
+                    swap(ptr1, ptr1->next);
+                    swapped = 1;
+                }
+                ptr1 = ptr1->next;
+            }
+            lptr = ptr1;
+        } while (swapped);
+    }
+        else if(way == 4){
+        do {
+            swapped = 0;
+            ptr1 = head;
+
+            while (ptr1->next != lptr) {
+                if (ptr1->getname() < ptr1->next->getname()) {
+                    swap(ptr1, ptr1->next);
+                    swapped = 1;
+                }
+                ptr1 = ptr1->next;
+            }
+            lptr = ptr1;
+        } while (swapped);
+    }
