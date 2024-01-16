351dedf8d4f4f47794b8ef076ba30afe75177d20
LOW JIE SHENG <129199459+jiesheng4616@users.noreply.github.com>
2024-01-16 23:14:46 +0800

    Update project.cpp

--- Submission/sec02/Nothing/Project/program/project.cpp
+++ Submission/sec02/Nothing/Project/program/project.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include<iomanip>
+#include<chrono>
+#include<thread>
 
 using namespace std;
 
@@ -11,7 +14,7 @@ class goods{
         string itemLocation;
     
     public:
-        goods(int id, string name, int price, string itemLocation){
+        goods(int id = 0, string name = "", int price = 0, string itemLocation = ""){
             this->id = id;
             this->name = name;
             this->price = price;
@@ -35,68 +38,6 @@ class goods{
         }
 };
 
-class itemNodeQueue{
-    public:
-        goods item;
-        itemNodeQueue *next;
-        itemNodeQueue *prev;
-};
-
-class itemQueue{
-    public:
-        itemNodeQueue *front;
-        itemNodeQueue *rear;
-
-        void createQueue(){
-            front = NULL;
-            rear = NULL;
-            size = 0;
-        }
-
-        bool isEmpty(){
-            if(front == NULL)
-                return true;
-            else
-                return false;
-        }
-
-        void enqueue(goods item){
-            itemNodeQueue *newNode = new itemNodeQueue;
-            newNode->item = item;
-            newNode->next = NULL;
-            newNode->prev = NULL;
-
-            if(isEmpty()){
-                front = newNode;
-                rear = newNode;
-            }else{
-                newNode->prev = rear;
-                rear->next = newNode;
-                rear = newNode;
-            }
-            size++;
-        }
-
-        void dequeue(){
-            if(isEmpty()){
-                cout << "Queue is empty" << endl;
-            }else{
-                itemNodeQueue *temp = front;
-                front = front->next;
-                front->prev = NULL;
-                delete temp;
-                size--;
-            }
-        }
-
-        goods getItem(){
-            if(isEmpty())
-                cout << "Queue is empty" << endl;
-            else
-                return front->item;
-        }
-};
-
 class historyNodeStack{
     public:
         goods item;
@@ -104,92 +45,3 @@ class historyNodeStack{
         historyNodeStack *next;
         historyNodeStack *prev;
 };
-
-class historyStack{
-    private:
-        historyNodeStack *top;
-        historyNodeStack *bottom;
-        int size;
-
-    public:
-        void createStack(){
-            top = NULL;
-            bottom = NULL;
-            size = 0;
-        }
-
-        bool isEmpty(){
-            if(top == NULL)
-                return true;
-            else
-                return false;
-        }
-    void push(goods item, char action){
-            historyNodeStack *newNode = new historyNodeStack;
-            newNode->item = item;
-            newNode->action = action;
-            newNode->next = NULL;
-            newNode->prev = NULL;
-
-            if(isEmpty()){
-                top = newNode;
-                bottom = newNode;
-            }else{
-                newNode->prev = top;
-                top->next = newNode;
-                top = newNode;
-            }
-            size++;
-        }
-
-        void pop(){
-            if(top == NULL){
-                cout << "Stack is empty" << endl;
-            }else{
-                historyNodeStack *temp = top;
-                top = top->prev;
-                top->next = NULL;
-                delete temp;
-                size--;
-            }
-        }
-
-        char getAction(){
-            if(isEmpty())
-                cout << "Stack is empty" << endl;
-            else
-                return top->action;
-        }
-
-        goods getItem(){
-            if(isEmpty())
-                cout << "Stack is empty" << endl;
-            else
-                return top->item;
-        }   
-};
-
-//main function
-
-itemQueue import(){
-    ifstream file;
-    int id;
-    string name;
-    int price;
-    string itemLocation;
-    itemQueue item;
-    goods newItem;
-    file.open("input.txt");
-    if(!file){
-        cout << "File not found" << endl;
-    }else{
-        //each line is a goods
-        while(file >> id >> name >> price >> itemLocation){
-            newItem = goods(id, name, price, itemLocation);
-            //insert into item
-            item.enqueue(newItem);
-        }
-    }
-    file.close();
-    return item;
-}
