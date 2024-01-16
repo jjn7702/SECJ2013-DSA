3d67409d2a6430d05d4cc6fa4c2b45dc4449e564
Chan Qing Yee <108549486+currylaksa@users.noreply.github.com>
2024-01-16 07:49:02 +0000

    arrange part

--- Submission/sec04/Group HSBC/Project/Project.cpp
+++ Submission/sec04/Group HSBC/Project/Project.cpp
@@ -6,9 +6,69 @@
 
 using namespace std;
 
+//printTitle() - WS
+
+//class UserAcc - WS
+
+//class Transaction - Hayden
+
+//ALL
+class BinaryTree(){ 
+
+};
+
+//read_data() - QY
+
+//read_Userdata() - CS
+
+//menuUser() - WS
+
+//menuStaff() - Hayden
+
+//calculate_add - WS
+
+//calculate_del - Hayden
+
+//userFile_update() - CS
+
+//menuFirstPage() - qy
+
+//role1() - CS
+
+//role2() - QY
+
+
 int main()
 {
-    
+    string filename1 = "inputProject.txt";
+    string filename2 = "userAcc.txt";
+
+    BinaryTree tree;
+    Transaction* account = new Transaction[99];
+	int count = read_data(account, filename1);
+	
+	for(int i=0 ; i < count ; i++){
+		tree.insertAccount(account[i]);
+	}
+
+    UserAcc userInfo[99];
+    int n = read_Userdata(userInfo, filename2);
+
+    int choice, role;
+    bool conti = true, repeat = true;
+
+    do{
+        role = menuFirstPage();
+
+        if(role == 1)
+            role1(userInfo, n, tree, account, count, choice, conti);
+        else if (role == 2)
+        	role2(userInfo, n, tree, account, count, choice, conti); 
+        else
+        	repeat = false;
+
+    }while(repeat == true);
+
     system("pause");
     return 0;
 }
