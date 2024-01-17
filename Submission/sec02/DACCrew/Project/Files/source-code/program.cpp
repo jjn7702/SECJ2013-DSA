62928f9c982fa21878174965272800d2e4ac5b82
Chai Yu Tong <96631869+Yutongchai@users.noreply.github.com>
2024-01-17 16:21:48 +0800

    Update program.cpp

--- Submission/sec02/DACCrew/Project/Files/source-code/program.cpp
+++ Submission/sec02/DACCrew/Project/Files/source-code/program.cpp
@@ -1,1 +1,21 @@
+//save to file function
+void saveToFile(stack &transactionStack)
+{
+    ofstream file("transaction_history.txt");
 
+    if (file.is_open())
+    {
+        nodeStack *temp = transactionStack.getTop();
+        while (temp)
+        {
+            file << temp->getDate() << "," << temp->getType() << "," << temp->getAmount() << "," << temp->getBalance() << "\n";
+            temp = temp->next;
+        }
+        file.close();
+        cout << "Transaction history saved to 'transaction_history.txt'." << endl;
+    }
+    else
+    {
+        cout << "Error opening the file." << endl;
+    }
+}
