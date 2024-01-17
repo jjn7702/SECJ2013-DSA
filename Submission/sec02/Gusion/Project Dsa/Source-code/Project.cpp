1d70c1c24213f5f834539e48d4a659abe310fa44
Muhammad Iman Firdaus <128123046+MuhammadImanFirdaus@users.noreply.github.com>
2024-01-18 00:18:30 +0800

    Update Project.cpp

--- Submission/sec02/Gusion/Project Dsa/Source-code/Project.cpp
+++ Submission/sec02/Gusion/Project Dsa/Source-code/Project.cpp
@@ -379,3 +379,322 @@ public:
    // <------------------------end admin functionality to remove for booking------------->
 
    //<----------------------------- END ADMIN FUNCTIONALITY------------------------------------>
+//<---------------------------------------DISPLAY INFORMATION FUNCTION-------------------------->
+   void displayAllBookings() const
+   {
+      cout << "\t\t---------------------------------------------------------------------------------------" << endl;
+      cout << left << setw(5) << "\t" << setw(15) << "  \tName"
+           << "  Date of Booking"
+           << "\t     Destination"
+           << "\t   Airlines"
+              "\t   IC Number"
+           << endl;
+      cout << "\t\t---------------------------------------------------------------------------------------" << endl;
+
+      displayQueueBookings(doneBooking);
+
+      cout << "\t\t---------------------------------------------------------------------------------------" << endl;
+   }
+
+   void displayRequestedBookings() const
+   {
+      cout << "\t\t---------------------------------------------------------------------------------------" << endl;
+      cout << left << setw(5) << "\t" << setw(15) << "  \tName"
+           << "  Date of Booking"
+           << "\t     Destination"
+           << "\t   Airlines"
+              "\t   IC Number"
+           << endl;
+      cout << "\t\t---------------------------------------------------------------------------------------" << endl;
+
+      displayQueueBookings(reservationRequests);
+
+      cout << "\t\t---------------------------------------------------------------------------------------" << endl;
+   }
+
+   void displayQueueBookings(const BookingQueue &queue) const
+   {
+      BookingQueue::Node *current = queue.getFrontNode(); // Use the public method to get the front node
+
+      while (current != nullptr)
+      {
+         current->data.displayBookingInfo(); // Display the booking info
+         current = current->next;            // Move to the next node
+      }
+   }
+   //<---------------------------------------END OF DISPLAY INFORMATION FUNCTION-------------------------->
+
+   // <------------------------function to update booking.txt------------->
+   void updateBookingFile()
+   {
+      // Open the booking.txt file for writing
+      ofstream writeFile("booking.txt");
+
+      if (!writeFile)
+      {
+         cout << "Cannot open file for writing." << endl;
+         return;
+      }
+
+      // Iterate through all done bookings and write them to the file
+      BookingQueue::Node *current = doneBooking.getFrontNode();
+      while (current != nullptr)
+      {
+         writeFile << current->data.getName() << " "
+                   << current->data.getDay() << " "
+                   << current->data.getMonth() << " "
+                   << current->data.getYear() << " "
+                   << current->data.getDestination() << " "
+                   << current->data.getAirline() << " "
+                   << current->data.getIcNum() << endl;
+
+         current = current->next;
+      }
+
+      // Close the file
+      writeFile.close();
+   }
+   // <------------------------end function to update booking.txt------------->
+};
+
+// <------------------------extract data from booking.txt------------->
+void extractData(BookingSystem &bookingSystem)
+{
+   string line;
+   fstream readFile("booking.txt", ios::in);
+
+   if (!readFile)
+   {
+      cout << "Cannot open file, something went wrong?\n";
+      return;
+   }
+   else
+   {
+      while (getline(readFile, line))
+      {
+         stringstream ss(line);
+         string name, destination, airline, icee;
+         int day, month, year;
+
+         ss >> name;
+         while (ss >> ws && !isdigit(ss.peek()))
+         {
+            string word;
+            ss >> word;
+            name += ' ' + word;
+         }
+
+         ss >> day;
+         ss.ignore();
+         ss >> month;
+         ss.ignore();
+         ss >> year;
+
+         ss >> destination >> airline >> icee;
+
+         if (!ss.fail())
+         {
+            Cust newBooking(name, day, month, year, destination, airline, icee);
+            bookingSystem.addBooking(newBooking);
+         }
+      }
+
+      // close file
+      readFile.close();
+   }
+}
+// <------------------------end extract data from booking.txt------------->
+
+// <------------------------start customer menu------------->
+void customerMenu(BookingSystem &bookingSystem)
+{
+   int choice;
+   do
+   {
+      cout << "\n\t\t\t\t   +================== CUSTOMER MENU ===================+" << endl;
+      cout << "\t\t\t\t   +1. \t\tRequest Reservation"
+           << "\t\t\t+" << endl;
+      cout << "\t\t\t\t   +2. \t\tCancel Reservation (Undo)"
+           << "\t\t+" << endl;
+      cout << "\t\t\t\t   +3. \t\tBack to Main Menu"
+           << "\t\t\t+" << endl;
+      cout << "\t\t\t\t   +====================================================+" << endl;
+      cout << "\n\n\t\t\t\t\t\tEnter your choice: ";
+
+      cin >> choice;
+
+      switch (choice)
+      {
+      case 1:
+      {
+         // Request Reservation
+         string name, destination, airline, ic;
+         int day, month, year;
+
+         cout << "\t\t\t\tEnter customer name: ";
+         cin.ignore();
+         getline(cin, name);
+
+         cout << "\n\t\t\t\tEnter date (day month year): " << endl;
+         cout << "\t\t\t\tDay : ";
+         cin >> day;
+         cout << "\t\t\t\tMonth :";
+         cin >> month;
+         cout << "\t\t\t\tYear :";
+         cin >> year;
+
+         cout << "\n\t\t\t\tEnter destination: ";
+         cin.ignore();
+         getline(cin, destination);
+
+         cout << "\n\t\t\t\tEnter airline: ";
+         getline(cin, airline);
+
+         cout << "\n\t\t\t\tEnter IC number: ";
+         cin >> ic;
+
+         // Create a Cust object with the provided details
+         Cust newBooking(name, day, month, year, destination, airline, ic);
+
+         // Enqueue the booking into reservationRequests
+         bookingSystem.reserveBooking(newBooking);
+
+         cout << "\n\t\t\t\tReservation request sent successfully." << endl;
+         break;
+      }
+      case 2:
+      {
+         // Cancel Reservation (Undo)
+         bookingSystem.cancelRecentReservation();
+         break;
+      }
+      case 3:
+         cout << "\t\t\t\tReturning to the main menu." << endl;
+         break;
+      default:
+         cout << "\t\t\t\tInvalid choice. Please try again." << endl;
+      }
+   } while (choice != 3);
+}
+// <------------------------end customer menu------------->
+
+// <------------------------start admin menu------------->
+void adminMenu(BookingSystem &bookingSystem)
+{
+
+   const string adminPassword = "admin123"; // Set the admin password
+
+   string enteredPassword;
+   char ch;
+
+   cout << "\n\t\t\t\tEnter admin password: ";
+
+   while ((ch = _getch()) != '\r') // '\r' is the Enter key
+   {
+      enteredPassword.push_back(ch);
+      cout << '*'; // Display asterisks instead of the actual characters
+   }
+
+   if (enteredPassword != adminPassword)
+   {
+      cout << "\n\t\t\t\tIncorrect password. Access denied." << endl;
+      return;
+   }
+
+   int choice;
+   do
+   {
+      cout << "\n\t\t\t\t   +================== ADMIN MENU ===================+" << endl;
+      cout << "\t\t\t\t   +1. \t\tApprove Reservation"
+           << "\t\t     +" << endl;
+      cout << "\t\t\t\t   +2. \t\tDisplay All Bookings"
+           << "\t\t     +" << endl;
+      cout << "\t\t\t\t   +3. \t\tBack to Main Menu"
+           << "\t\t     +" << endl;
+      cout << "\t\t\t\t   +4. \t\tSearch Customer"
+           << "\t\t\t     +" << endl;
+      cout << "\t\t\t\t   +5. \t\tRemove Booking"
+           << "\t\t\t     +" << endl;
+      cout << "\t\t\t\t   +=================================================+" << endl;
+      cout << "\n\n\t\t\t\t\t\tEnter your choice: ";
+      cin.ignore();
+      cin >> choice;
+
+      string icToSearch;
+      string icNumToRemove;
+
+      switch (choice)
+      {
+      case 1:
+         // Approve Reservation (Dequeue)
+         // ...
+         bookingSystem.approveReservation();
+         break;
+      case 2:
+         // Display All Bookings
+         bookingSystem.displayAllBookings();
+         break;
+      case 3:
+         cout << "\t\t\t\tReturning to the main menu." << endl;
+         break;
+      case 4:
+         // string icToSearch;
+         cout << "\n\t\t\t\tEnter customer IC number to search: ";
+         cin >> icToSearch;
+         bookingSystem.searchByICNumber(icToSearch);
+         break;
+      case 5:
+         // string icNumToRemove;
+         cout << "\t\t\t\tEnter customer IC number to remove from booking: ";
+         cin >> icNumToRemove;
+         bookingSystem.removeBookingByIC(icNumToRemove);
+         break;
+
+      default:
+         cout << "\t\t\t\tInvalid choice. Please try again." << endl;
+      }
+   } while (choice != 3);
+}
+// <------------------------end admin menu------------->
+
+int main()
+{
+   BookingSystem bookingSystem;
+   extractData(bookingSystem);
+
+   int userType;
+   do
+   {
+      cout << "\n\t\t\t\t   +================================================+" << endl;
+      cout << "\n\t\t\t\t   +     WELCOME TO AIRLINE RESERVATION SYSTEM      +" << endl;
+      cout << "\n\t\t\t\t   +================== MAIN MENU ===================+" << endl;
+      cout << "\t\t\t\t   +1. \t\t\tADMIN/STAFF"
+           << "\t\t    +" << endl;
+      cout << "\t\t\t\t   +2. \t\t\tCUSTOMER"
+           << "\t\t    +" << endl;
+      cout << "\t\t\t\t   +3. \t\t\tExit"
+           << "\t\t\t    +" << endl;
+      cout << "\t\t\t\t   +================================================+" << endl;
+      cout << "\n\n\t\t\t\t\t\tEnter your choice: ";
+      cin >> userType;
+
+      switch (userType)
+      {
+      case 1:
+         // Admin
+         adminMenu(bookingSystem);
+         break;
+      case 2:
+         // Customer
+         customerMenu(bookingSystem);
+         break;
+      case 3:
+         cout << "\n\t\t\t\tExiting the program." << endl;
+         break;
+      default:
+         cout << "\n\t\t\t\tInvalid choice. Please try again." << endl;
+      }
+   } while (userType != 3);
+   system("pause");
+   return 0;
+}
