5add52cf60742134baa50e88fc759f9261f60071
Kugen_gen <kugen@graduate.utm.my>
2024-01-17 22:37:42 +0800

    Update project.cpp

--- Submission/sec02/Tempest/Project/source_code/project.cpp
+++ Submission/sec02/Tempest/Project/source_code/project.cpp
@@ -115,7 +115,19 @@ public:
         return count == N;
     }
 
-   
+    void enqueue(const TaskData &newTask)
+    {
+        if (isfull())
+        {
+            cout << "Sorry, the queue is full" << endl;
+        }
+        else
+        {
+            rear = (rear + 1) % N; //*
+            data[rear] = newTask;
+            count++;
+        }
+    }
 
     void dequeue()
     {
